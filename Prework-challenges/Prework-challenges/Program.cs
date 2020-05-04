22375413c0bc4b94aa01c6bb25a95f89f67a4bc3
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-03 19:39:43 -0500

    setting up vs

---
+++ Prework-challenges/Prework-challenges/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prework_challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
